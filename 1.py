5e53c8964a37180ea3a49d3f4ccd12f76e9994e5
alekhya-alex <136616936+alekhya-alex@users.noreply.github.com>
2024-05-23 23:03:33 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,1 +1,10 @@
-
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> if __name__ == '__main__':
+...     a = int(input())
+...     b = int(input())
+...     c=a+b
+...     c1=a-b
+...     c2=a*b
+...     print(c)
+...     print(c1)
