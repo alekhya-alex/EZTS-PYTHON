b9611ad744bd9a1c1753224beee61ab47d49c196
alekhya-alex <136616936+alekhya-alex@users.noreply.github.com>
2024-05-31 09:44:37 +0530

    Add files via upload

---
+++ hackerrank/1.py
@@ -0,0 +1,12 @@
+def split_and_join(line):
+    a = line.split(" ")
+
+    b = "-".join(a)
+
+    return(b)
+    # write your code here
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
